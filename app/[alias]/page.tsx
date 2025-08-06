a44515b2397c293db34137e2a8a67181464d6e46
Ruby Chen <rc071404@bu.edu>
2025-08-05 20:01:22 -0400

    debugged [alias]/page

--- app/[alias]/page.tsx
+++ app/[alias]/page.tsx
@@ -1,10 +1,11 @@
 import { redirect } from "next/navigation";
 import getCollection, {URLS_COLLECTION} from "@/lib/mangodb";
 
-export default async function AliasPage({ params }: { params: { alias: string } }) {
+export default async function AliasPage({ params }: { params: { alias: string }}) {
+    const { alias } = await params;
 
     const urlsCollection = await getCollection(URLS_COLLECTION);
-    const new_url = await urlsCollection.findOne({ alias: params.alias });
+    const new_url = await urlsCollection.findOne({ alias });
 
     if (new_url) {
         redirect(new_url.url);
